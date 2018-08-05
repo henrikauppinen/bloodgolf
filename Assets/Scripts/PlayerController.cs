12fecd252f5409184091b51bd07384ff12b57849
Henri Kauppinen <kauppinen.henri@gmail.com>
2018-08-05 23:30:49 +0300

    Repo setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+  [SerializeField] float speed = 0.0f;
+  [SerializeField] float maxSpeed = 10.0f;
+  [SerializeField] float enginePower = 0.06f;
+  [SerializeField] float brakePower = 0.08f;
+  [SerializeField] float windResistancePower = 0.03f;
+
+  void Update() {
+    float steeringInput = Input.GetAxis("Horizontal") * Time.deltaTime * 120.0f;
+
+    float throttle = Input.GetAxis("Vertical");
+
+    if (throttle > 0) {
+      accelerate();
+    }
+    else if (throttle < 0) {
+      decelerate();
+    }
+    else {
+      coast();
+    }
+
+    move();
+    steer(steeringInput);
+  }
+
+  public void accelerate() {
+    speed = speed >= maxSpeed ? maxSpeed : speed + enginePower;
+  }
+
+  public void decelerate() {
+    float newSpeed = speed - brakePower;
+    speed = newSpeed < 0 ? 0 : newSpeed;
+  }
+
+  public void coast() {
+    float newSpeed = speed - windResistancePower;
+    speed = newSpeed < 0 ? 0 : newSpeed;
+  }
+
+  public void move() {
+    transform.Translate(0, 0, speed * Time.deltaTime);
+  }
+
+  public void steer(float steeringInput) {
+    transform.Rotate(0, steeringInput, 0);
+  }
+}
---
+++ Assets/Tests/PlayerControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerControllerTest {
+
+  [Test]
+  public void testCorrectSteeringOnReverse() {
+    var car = new PlayerController();
+  }
+}
